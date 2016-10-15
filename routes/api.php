bc57df751e66be13ebbcabce2e6f2434aa37cf9d
CreativeKoen <CreativeKoen@gmail.com>
2016-10-15 21:49:49 +0200

    added the first api routes

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+
+    $api->get('test', 'App\Http\Controllers\MainController@index');
+    $api->get('user/{userID}/role/{roleID}', 'App\Http\Controllers\MainController@attachUserRole');
+
+});
