8497d08eefdbd97cec7233eb492ca1305931edb2
CreativeKoen <CreativeKoen@gmail.com>
2016-10-22 16:08:14 +0200

    added get user role function to get the role of a user, added a attach user role function to give a user a role, and added a function to add permissions to a role

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\User;
+
 use App\Role;
+use App\Permission;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
@@ -16,15 +18,48 @@ class MainController extends Controller
         return $this->response->array($user);
     }
 
-
-    public function attachUserRole($userID, $roleID)
+    public function getUserRole($userID)
     {
+        $user = User::findOrFail($userID);
+        return $this->response->array($user->roles);
+    }
+
+    public function getPermissions(int $roleID)
+    {
+        $role = Role::findOrFail($roleID);
+
+        return $this->response->array($role->permissions);
+    }
+
+
+    public function attachUserRole(Request $request)
+    {
+        $parameter = $request->only(['role', 'user']);
+
+        $roleID = (int) $parameter['role'];
+        $userID = (int) $parameter['user'];
+
         $user = User::findOrFail($userID);
         $role = Role::findOrFail($roleID);
 
         $user->roles()->attach($role);
 
-        return $this->response->array(['user'=> $user->name, 'role' => $role->name]);
+        return $this->response->created();
     }
 
+    public function attachPermission(Request $request)
+    {
+        $parameter = $request->only(['permission', 'role']);
+
+        $roleID = (int) $parameter['role'];
+        $permissionID = (int) $parameter['permission'];
+
+        $role = Role::findOrFail($roleID);
+        $permission = Permission::findOrFail($permissionID);
+
+        $role->attachPermission($permission);
+
+        return $this->response->created();
+
+    }
 }
