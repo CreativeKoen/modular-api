480b0bc3e49e33d100831393610c03b29928d907
CreativeKoen <CreativeKoen@gmail.com>
2016-10-22 15:42:03 +0200

    added some routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,19 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
 
-    $api->get('test', 'App\Http\Controllers\MainController@index');
-    $api->get('user/{userID}/role/{roleID}', 'App\Http\Controllers\MainController@attachUserRole');
+
+    $api->post('authenticate', 'App\Http\Controllers\AuthController@authenticate');
+
+
+    # getting data
+    $api->get('users', 'App\Http\Controllers\MainController@index');
+    $api->get('users/{userID}', 'App\Http\Controllers\MainController@getUserRole');
+    $api->get('users/{userID}/roles', 'App\Http\Controllers\MainController@getUserRole');
+
+    $api->get('role/{roleID}/permissions', 'App\Http\Controllers\MainController@getPermissions');
+
+    # setting data
+    $api->post('users/role/add', 'App\Http\Controllers\MainController@attachUserRole');
+    $api->post('role/permission/add', 'App\Http\Controllers\MainController@attachPermission');
 
 });
